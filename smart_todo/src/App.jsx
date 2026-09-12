38648ae88bb9155f9dba6969be2456859e43a0af
dhruvgalhotra <dhruvgalhotra72@gmail.com>
2026-09-12 23:35:58 +0530

    Add navbar component

--- smart_todo/src/App.jsx
+++ smart_todo/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
+
       <h1>Smart To-Do Planner</h1>
     </div>
   )
---
+++ smart_todo/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h2>Smart Planner</h2>
+            <p>My Daily Planner</p>
+        </nav>
+    )
+}
+
+export default Navbar
